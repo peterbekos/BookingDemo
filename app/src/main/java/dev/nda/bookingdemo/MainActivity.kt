cb22e893c26da3624af429de35d7658f63d5298f
pete bekos <peterbekos@gmail.com>
2021-08-11 17:36:49 -0500

    get rooms to show in a list

--- app/src/main/java/dev/nda/bookingdemo/MainActivity.kt
+++ app/src/main/java/dev/nda/bookingdemo/MainActivity.kt
@@ -2,10 +2,25 @@ package dev.nda.bookingdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import dev.nda.bookingdemo.adapter.RoomAdapter
+import dev.nda.bookingdemo.model.RoomModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        room_recycler.layoutManager = LinearLayoutManager(this)
+        room_recycler.adapter = RoomAdapter(
+            listOf(
+                RoomModel("Room A", 10, "https://images.unsplash.com/photo-1571624436279-b272aff752b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1504&q=80"),
+                RoomModel("Room B", 1, "https://images.unsplash.com/photo-1571624436279-b272aff752b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1504&q=80"),
+                RoomModel("Room C", 5, "https://images.unsplash.com/photo-1571624436279-b272aff752b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1504&q=80"),
+                RoomModel("Room D", 0, "https://images.unsplash.com/photo-1571624436279-b272aff752b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1504&q=80"),
+                RoomModel("Room E", 10, "https://images.unsplash.com/photo-1571624436279-b272aff752b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1504&q=80"),
+            ),
+            resources
+        )
     }
 }
--- app/src/main/java/dev/nda/bookingdemo/adapter/RoomAdapter.kt
+++ app/src/main/java/dev/nda/bookingdemo/adapter/RoomAdapter.kt
@@ -1,30 +1,42 @@
 package dev.nda.bookingdemo.adapter
 
+import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import dev.nda.bookingdemo.R
 import dev.nda.bookingdemo.model.RoomModel
 
-class RoomAdapter(val rooms: List<RoomModel>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class RoomAdapter(
+    private val rooms: List<RoomModel>,
+    private val resources: Resources
+) : RecyclerView.Adapter<RoomAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_room, parent, false)
         return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val room = rooms[position]
+        Picasso.get().load(room.thumbnail).into(holder.image)
+        holder.name.text = room.name
+        holder.availability.text = resources.getQuantityString(
+            R.plurals.spots_remaining,
+            room.spots ?: 0,
+            room.spots ?: 0
+        )
     }
 
     override fun getItemCount(): Int {
         return rooms.size
     }
 
-    class ViewHolder(val itemView: View): RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val image = itemView.findViewById<ImageView>(R.id.room_image)
         val name = itemView.findViewById<TextView>(R.id.room_name_text)
         val availability = itemView.findViewById<TextView>(R.id.spots_available_text)
